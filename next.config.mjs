8fc90c6ab48e46c588f944838db87af06e5924bd
Thiago Dal Santo <thiagodalsantodaluz@gmail.com>
2026-04-22 22:49:48 +0100

    DEPLOY: Github Actions to Github Pages

--- next.config.mjs
+++ next.config.mjs
@@ -2,11 +2,14 @@ import { withContentlayer } from "next-contentlayer";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
-	output: "standalone",
-	experimental: {
-		mdxRs: true,
-	},
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+  experimental: {
+    mdxRs: true,
+  },
 };
 
 export default withContentlayer(nextConfig);
