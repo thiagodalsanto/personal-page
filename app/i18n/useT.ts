240eb5343ce8f46fa554937291af4f71d4b91582
Thiago Dal Santo <thiagodalsantodaluz@gmail.com>
2026-04-24 05:06:58 +0100

    CHORE: Deleting and recreating files reorganizing folders

--- app/i18n/useT.ts
+++
@@ -1,8 +0,0 @@
-import { useSettings } from "../store/useSettings";
-import { translations } from "./index";
-
-export function useT() {
-  const lang = useSettings((s) => s.lang);
-
-  return translations[lang];
-}
