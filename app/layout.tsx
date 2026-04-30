6e6b9436948f0b096855b371c9ced6005b0678ac
Thiago Dal Santo <thiagodalsantodaluz@gmail.com>
2026-04-30 23:11:23 +0100

    CHORE: Layout with personal-page for icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,9 +12,9 @@ export const metadata: Metadata = {
   },
   description: "Web & Mobile Developer with 4+ years of experience",
   icons: {
-    icon: "favicon.png",
-    shortcut: "favicon.png",
-    apple: "favicon.png",
+    icon: "/personal-page/favicon.png",
+    shortcut: "/personal-page/favicon.png",
+    apple: "/personal-page/favicon.png",
   },
 };
 
