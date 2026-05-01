671f84f1d57086ebaf4440ee70b587ae543aebda
Thiago Dal Santo <thiagodalsantodaluz@gmail.com>
2026-05-01 02:59:26 +0100

    FIX: Next Config:

--- next.config.mjs
+++ next.config.mjs
@@ -2,15 +2,11 @@ import { withContentlayer } from "next-contentlayer";
 
 /** @type {import('next').NextConfig} */
 
-const repo = "personal-page";
-
 const nextConfig = {
   output: "export",
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === "production" ? `/${repo}` : "",
-  assetPrefix: process.env.NODE_ENV === "production" ? `/${repo}/` : "",
   pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
   experimental: {
     mdxRs: true,
