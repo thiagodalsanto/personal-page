7df31e662a1e32fca6ed8747e986bcb98757d0bb
Thiago Dal Santo <thiagodalsantodaluz@gmail.com>
2026-06-25 10:05:00 +0100

    CHORE: Changing Deploy to Vercel for use the force-dynamics

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,6 @@ import { withContentlayer } from "next-contentlayer";
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  output: "export",
   images: {
     unoptimized: true,
   },
